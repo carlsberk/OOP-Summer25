9b20b2e5a0f07c6e92e1cd6c9ed71c23c2da5a40
carlsberk <137445298+carlsberk@users.noreply.github.com>
2025-03-13 13:55:00 +0100

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,6 @@
+first_name = "Berk"
+last_name = "Sarıtaş"
+index_number = "35236"
+nationality = "Turkish"
+starting_date = "02.02.2025"
+courses = "Polish,Logistic,Mathematics,Objected-Oriented Programing"
