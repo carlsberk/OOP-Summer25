02b053dffad1e5cdc224cb626c5f39211500c53c
carlsberk <137445298+carlsberk@users.noreply.github.com>
2025-03-13 14:30:00 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -8,4 +8,9 @@ student1 = {
     'courses': ['Polish', 'Logistics', 'Mathematics', 'Object-Oriented Programming']
 }
 
-print(student1)
+print(student1['first_name'])
+print(student1['last_name'])
+print(student1['index_number'])
+print(student1['nationality'])
+print(student1['starting_date'])
+print(student1['courses'])
