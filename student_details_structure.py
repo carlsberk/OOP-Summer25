ce1d041c79453b0bc181c2668a3f9118a0ac7ce6
carlsberk <137445298+carlsberk@users.noreply.github.com>
2025-03-13 14:20:30 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,10 +1,11 @@
-# example
-student1 = {first_name = "Berk"
-last_name = "Sarıtaş"
-index_number = "35236"
-nationality = "Turkish"
-starting_date = "02.02.2025"
-courses = "Polish,Logistic,Mathematics,Objected-Oriented Programing"
- 'first_name':'Berk',
-  'last_name':'Sarıtaş'
+#Example
+student1 = {
+    'first_name': 'Berk',
+    'last_name': 'Sarıtaş',
+    'index_number': '35236',
+    'nationality': 'Turkish',
+    'starting_date': '02.02.2025',
+    'courses': ['Polish', 'Logistics', 'Mathematics', 'Object-Oriented Programming']
 }
+
+print(student1)
